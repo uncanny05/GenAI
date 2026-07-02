64cbb14b344688b11ee1cc4ed87aee807b0ed947
uncanny05 <simplyjay0505@gmail.com>
2026-07-02 15:39:04 +0100

    getAllPosts Controller created

---
+++ server/controllers/Posts.js
@@ -0,0 +1,21 @@
+import Post from "../models/Posts.js";
+import * as dotenv from "dotenv";
+import { createError } from "../error.js";
+import { v2 as cloudinary } from "cloudinary";
+
+dotenv.config();
+
+// Get all posts
+export const getAllPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find({});
+    return res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    next(
+      createError(
+        error.status,
+        error?.response?.data?.error?.message || error?.message,
+      ),
+    );
+  }
+};
