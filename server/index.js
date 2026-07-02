6e5fbd1036f53c355f0132fa32b03994c999ec33
uncanny05 <simplyjay0505@gmail.com>
2026-07-02 14:50:57 +0100

    backend & MongoDB setup

---
+++ server/index.js
@@ -0,0 +1,48 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+//  error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({ success: false, status, message });
+});
+
+// Default get
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello Develpoers!",
+  });
+});
+
+//  function to connect to mongoDB
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => {
+      console.error("Failed to connect to DB");
+    });
+};
+
+// function to start the server
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
