9c0fc7ce604a4bb54c560944a2243ebc7eb7d693
uncanny05 <simplyjay0505@gmail.com>
2026-07-05 00:12:17 +0100

    Minor text correction

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use("/api/generateImage", generateImage);
 // Default get
 app.get("/", async (req, res) => {
   res.status(200).json({
-    message: "Hello Develpoers!",
+    message: "Hello Developers!",
   });
 });
 
